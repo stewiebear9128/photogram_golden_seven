71a1656519c647039e5239447068e21b0e61f47c
stewiebear9128 <scott9128@gmail.com>
2015-05-10 01:08:11 -0500

    updated with all CRUD finished

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,33 @@ class PhotosController < ApplicationController
   def index
     @list_of_photos = Photo.all
   end
+  def show
+    @current_photo = Photo.find_by({ :id => params["id"]})
 
-
+  end
+  def new_form
+  end
+  def create_row
+    i = Photo.new
+    i.source = params["the_source"]
+    i.caption = params["the_caption"]
+    i.save
+    redirect_to("http://localhost:3000/photos")
+  end
+  def destroy
+    i = Photo.find(params["id"])
+    i.destroy
+     redirect_to("http://localhost:3000/photos")
+  end
+  def edit_form
+    @id = params["id"]
+    @current_photo = Photo.find_by(@id)
+  end
+  def update_photo
+      i = Photo.find(params["id"])
+    i.source = params["the_source"]
+    i.caption = params["the_caption"]
+    i.save
+    redirect_to("http://localhost:3000/photos/"+@id.to_s)
+  end
 end
